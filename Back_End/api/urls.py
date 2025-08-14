3c6de8fe0919218a53cac565032337a82bd1c44f
LeticiaRoth <leticiaroth1@gmail.com>
2025-08-14 16:52:53 -0300

    Atualizações do projeto

--- Back_End/api/urls.py
+++ Back_End/api/urls.py
@@ -4,6 +4,7 @@ from .views import *
 #Crio os endpoints
 urlpatterns = [
     path('autores', AutoresView.as_view()) ,#URLS para CRUD no banco de dados
-    path('crud/<int:pk>/', AutoresCrud.as_view())
+    path('crud/<int:pk>/', AutoresCrud.as_view()),
+    path('authors', visualizacao_autor )
 ]
 
--- Back_End/api/views.py
+++ Back_End/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render #Renderiza
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Autor
 from .serializers import AutorSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 #Serve como um post, e o list como get
 class AutoresView(ListCreateAPIView):
@@ -13,3 +16,17 @@ class AutoresView(ListCreateAPIView):
 class AutoresCrud(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+
+@api_view(["GET", "PUSH"])
+def visualizacao_autor(request):
+    if request.method == "GET": 
+        queryset = Autor.objects.all()
+        serializer = AutorSerializer(queryset, many = True)
+        return (serializer.data)
+    elif request.method == "POST":
+        serializer = AutorSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
